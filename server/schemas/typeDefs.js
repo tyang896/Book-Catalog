d556d9e8782b3258aed83d2e7ddece078731cefc
Talee Y <talee.yng@gmail.com>
2022-12-03 15:24:40 -0600

    Add types to typeDefs.js file

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,38 @@
+const { gql } = require('apollo-server-express');
+
+//TODO: Add code for saveBook() Mutation
+//TODO: Add code for bookId, authors, image, and link in Book type
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: ID//NOTE: Change the ID value to what is returned from Google Books API
+        authors: [String]
+        description: String
+        title: String
+        image:
+        link:
+
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Mutations {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook()
+        removeBook(bookId: ID!): User
+    }
+`
