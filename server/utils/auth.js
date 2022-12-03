742d41e1f26c311f22a4e40748d3415f11dfee55
Talee Y <talee.yng@gmail.com>
2022-12-03 03:11:42 -0600

    Add new package to server/package.json file and update auth middleware function

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,17 +6,18 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function (req) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
 
+    //return the request object of no token exists
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
     // verify token and get user data out of it
@@ -25,11 +26,10 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    next();
+    // return the request object
+    return req
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
